7c3af9d9169ffff1a28c2e3aa0543c2ffed7988c
KanamiQwQ <kanami0619@163.com>
2026-03-29 18:50:27 +0800

    源码逻辑优化

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import random
 import difflib
 import string
 import unicodedata
-from astrbot.api.all import *
-from astrbot.api.event import AstrMessageEvent
-from astrbot.api.event.filter import command, regex
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 
 @register("sayoroll", "mas-alone", "随机数字或随机事件", "1.0.0")
@@ -15,20 +13,22 @@ class SayoRoll(Star):
 
     def normalize_str(self, s: str) -> str:
         return unicodedata.normalize('NFKC', s)
-    @regex(r"^(.*?)概率$")
+
+    @filter.regex(r"^(.*?)概率$")
     async def on_probability(self, event: AstrMessageEvent):
         """处理后缀为“概率”的消息"""
         user_input = event.message_str.strip()
-
         replaced_input = user_input.replace('我', '你')
+        
         probability = random.uniform(0.01, 100.00)
         msg = f'{replaced_input}为：{probability:.2f}%'
         yield event.plain_result(msg)
 
-    @command("roll")
+    @filter.command("roll")
     async def on_roll(self, event: AstrMessageEvent, message: str = ""):
         """处理 /roll 指令"""
         args_text = message.strip()
+
         if not args_text:
             msg = f'{random.randint(1, 100)}'
             
@@ -39,12 +39,11 @@ class SayoRoll(Star):
             else:
                 msg = f'{random.randint(1, num)}'
                 
-        elif re.search(r'([\u4e00-\u9fff])[不没].*?\1(.*?)', args_text):
-            result = re.search(r'([\u4e00-\u9fff])[不没].*?\1(.*?)', args_text)
+        elif result := re.search(r'([\u4e00-\u9fff])[不没]\1(.*?)', args_text):
             options = [result.group()[:1], result.group()[1:]]
             
-            first_params = args_text[:result.span()[0]].replace('我', 'temp').replace('temp', '你')
-            second_params = args_text[result.span()[1]:].replace('我', 'temp').replace('temp', '你')
+            first_params = args_text[:result.span()[0]].replace('我', '你')
+            second_params = args_text[result.span()[1]:].replace('我', '你')
             
             similarity = difflib.SequenceMatcher(None, first_params.lower(), second_params.lower()).ratio()
             if similarity > 0.8:
@@ -58,28 +57,24 @@ class SayoRoll(Star):
             if similarity > 0.8:
                 msg = '总共就2个参数..还这么相似..怎么roll都一样啊'
             else:
-                msg = f'当然是{random.choice(options)}咯'
-                msg = msg.replace('我', 'temp').replace('temp', '你')
+                msg = f'当然是{random.choice(options)}咯'.replace('我', '你')
                 
         else:
             args_text = self.normalize_str(args_text)
-            args_without_punctuation = args_text.translate(str.maketrans('', '', string.punctuation))
             
-            if len(args_without_punctuation.split(' ')) == 1:
+            options = args_text.split()
+            
+            if len(options) <= 1:
                 msg = '未匹配到参数！'
             else:
-                options = [x for x in args_text.split(' ') if x.strip()]
-                if len(options) > 1:
-                    similarities = [
-                        difflib.SequenceMatcher(None, option1.lower(), option2.lower()).ratio() 
-                        for i, option1 in enumerate(options) 
-                        for option2 in options[i+1:]
-                    ]
-                    if any(similarity > 0.8 for similarity in similarities):
-                        msg = f'总共就{len(options)}个参数..还这么相似..怎么roll都一样啊'
-                    else:
-                        msg = f'当然是{random.choice(options)}咯'
+                similarities = [
+                    difflib.SequenceMatcher(None, option1.lower(), option2.lower()).ratio() 
+                    for i, option1 in enumerate(options) 
+                    for option2 in options[i+1:]
+                ]
+                if any(similarity > 0.8 for similarity in similarities):
+                    msg = f'总共就{len(options)}个参数..还这么相似..怎么roll都一样啊'
                 else:
-                    msg = '未匹配到参数！'
+                    msg = f'当然是{random.choice(options)}咯'
 
         yield event.plain_result(msg)
